b68eef591efd850b4b43db62d0a803d857c6e293
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-02 14:01:58 +0300

    added random number function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Title from '../components/Title';
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
 export default function GameScreen() {
   return (
     <View style={styles.screen}>
