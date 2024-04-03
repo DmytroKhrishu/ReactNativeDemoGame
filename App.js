fa675b186433c1f1e4bedd2f530bd1ac6033317a
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-03 17:06:16 +0300

    added game over logic

--- App.js
+++ App.js
@@ -6,24 +6,40 @@ import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function numberPickerHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
   }
 
   let screen = <StartGameScreen onNumberPicked={numberPickerHandler} />;
 
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} />;
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
   }
 
   return (
     <>
       <StatusBar style="light" />
-      <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.rootScreen}
+      >
         <ImageBackground
           source={require('./assets/images/background.png')}
           resizeMode="cover"
