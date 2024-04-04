6430a3065d0101a323b39afbb543c400bd31340a
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-04 14:07:55 +0300

    added GuessLogItem compoment and finished log output

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,7 @@ import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
+import GuessLogItem from '../components/game/GuessLogItem';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -61,6 +62,8 @@ export default function GameScreen({ userNumber, onGameOver }) {
     setGuessRounds((prev) => [newRndNumber, ...prev]);
   }
 
+  const guessRoundsListLength = guessRounds.length;
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's guess</Title>
@@ -82,11 +85,15 @@ export default function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
-        {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+      <View style={styles.logListContainer}>
         <FlatList
           data={guessRounds}
-          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          renderItem={(itemData) => (
+            <GuessLogItem
+              guess={itemData.item}
+              roundNumber={guessRoundsListLength - itemData.index}
+            />
+          )}
           keyExtractor={(item) => item}
         />
       </View>
@@ -98,7 +105,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 20,
-    marginTop: 150,
+    marginTop: 50,
   },
   instructionText: {
     marginBottom: 12,
@@ -108,5 +115,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  logListContainer: {
+    flex: 1,
+    padding: 16
   },
 });
